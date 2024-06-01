4021e456418e1b412c0cc64e9f60e88b0e55c946
cmlosariaGIS <62094459+cmlosariaGIS@users.noreply.github.com>
2024-06-01 17:39:17 +0700

    Update browserbehaviour.js

--- browserbehaviour.js
+++ browserbehaviour.js
@@ -1,4 +1,4 @@
 function preventSwipeRefresh(){let touchStartY=0;document.addEventListener('touchstart',(event)=>{touchStartY=event.touches[0].clientY});document.addEventListener('touchmove',(event)=>{const touchEndY=event.touches[0].clientY;const deltaY=touchEndY-touchStartY;if(deltaY>0&&window.scrollY===0){event.preventDefault()}})}
-preventSwipeRefresh();function handleBackNavigation(){window.addEventListener('popstate',function(event){showBackConfirmation()});window.addEventListener('keyup',function(event){if(event.key==='Escape'||event.key==='ArrowLeft'){showBackConfirmation()}})}
-function showBackConfirmation(){if(confirm("Are you sure you want to exit Midpoint 🥹👉👈?")){window.history.back()}else{history.pushState(null,null,window.location.href)}}
-handleBackNavigation()
+preventSwipeRefresh();function handleBackNavigation(){window.addEventListener('popstate',function(event){showBackConfirmation('browserBack')});window.addEventListener('keyup',function(event){if(event.key==='Escape'||event.key==='ArrowLeft'){showBackConfirmation('keyboard')}})}
+function showBackConfirmation(source){if(source!=='shareButton'&&confirm("Are you sure you want to exit Midpoint 🥹👉👈?")){window.history.back()}else{history.pushState(null,null,window.location.href)}}
+history.pushState(null,null,window.location.href);handleBackNavigation()
